23343d08bce2fc1dc5f4e41a9e5239c04c5ffee6
joaooliveiraa11 <up202108737@edu.fe.up.pt>
2023-10-25 22:12:04 +0100

    move piece done without validation

--- TP1/logic.pl
+++ TP1/logic.pl
@@ -1,25 +1,30 @@
+% move(+GAMESTATE, +FROM, +TO, -NEWGAMESTATE)
 move(GAMESTATE, FROM, TO, NEWGAMESTATE) :-
     get_piece(GAMESTATE, FROM, PIECE),
     add_piece(GAMESTATE, FROM, 0, TEMPSTATE),
-    display_game(TEMPSTATE),
     add_piece(TEMPSTATE, TO, PIECE, NEWGAMESTATE).
 
-% get_piece(+GameState, +Coords, -Piece)
+% get_piece(+GAMESTATE, +(X-Y), -PIECE)
 get_piece(GAMESTATE, X-Y, PIECE) :-
     nth1(X, GAMESTATE, ROW),
     nth1(Y, ROW, PIECE).
 
-% add_piece(+GameState, +Coords, +Piece, -NewGameState)
+% add_piece(+GAMESTATE, +(X-Y), +PIECE, -NEWGAMESTATE)
 add_piece(GAMESTATE, X-Y, PIECE, NEWGAMESTATE) :-
     get_row(GAMESTATE, X, ROW),
-    replace_at(Y, ROW, PIECE, NEWROW),
-    replace_at(X, GAMESTATE, NEWROW, NEWGAMESTATE).
+    replace_piece(Y, ROW, PIECE, NEWROW),
+    replace_row(X, GAMESTATE, NEWROW, NEWGAMESTATE).
 
-% get_row(+GameState, +INDEX, -SelectedRow)
+% get_row(+GAMESTATE, +INDEX, -SELECTEDROW)
 get_row(GAMESTATE, INDEX, SELECTEDROW) :-
     nth1(INDEX, GAMESTATE, SELECTEDROW).
 
-% replace_at(+Index, +List, +Element, -NewList)
-replace_at(INDEX, LIST, ELEMENT, NEWLIST) :-
-    nth1(INDEX, LIST, ELEMENT, NEWLIST).
+% replace_piece(+INDEX, +ROW, +PIECE, -NEWROW)
+replace_piece(INDEX, ROW, PIECE, NEWROW) :-
+    nth1(INDEX, ROW, _, TEMPROW),  % Discard the old element at Index
+    nth1(INDEX, NEWROW, PIECE, TEMPROW).
 
+% get_row(+INDEX, +BOARD, +PIECE, -NEWBOARD)
+replace_row(INDEX, BOARD, PIECE, NEWBOARD) :-
+    nth1(INDEX, BOARD, _, TEMPBOARD),  % Discard the old element at Index
+    nth1(INDEX, NEWBOARD, PIECE, TEMPBOARD).
