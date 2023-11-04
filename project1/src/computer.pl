2af65a24a171e4cb76747fc4cd2e4c3e33c465be
joaooliveiraa11 <up202108737@edu.fe.up.pt>
2023-11-04 19:10:07 +0000

    fixed bot predicates

--- project1/src/computer.pl
+++ project1/src/computer.pl
@@ -20,37 +20,37 @@ random_choice(CHOICE, OPTIONS) :-
     random(0, LENGTH, INDEX),
     nth0(INDEX, OPTIONS, CHOICE).
 
-choose_move(GAMESTATE, 1, MOVE) :-
-    valid_moves(GAMESTATE, VALIDMOVES),
+choose_move(GAMESTATE, PLAYER, 1, MOVE) :-
+    valid_moves(GAMESTATE, PLAYER, VALIDMOVES),
     random_choice(MOVE, VALIDMOVES),
     MOVE = [YS-XS, YF-XF],
     format('Computer randomly moved from (~w-~w) to (~w-~w).', [YS, XS, YF, XF]),nl.
 
-choose_move(GAMESTATE, 2, MOVE) :-
-    valid_moves(GAMESTATE, VALIDMOVES),
-    choose_best_move(GAMESTATE, VALIDMOVES, MOVE),
-    % wait_execute,
-    sleep(3),
+choose_move(GAMESTATE, PLAYER, 2, MOVE) :-
+    valid_moves(GAMESTATE, PLAYER, VALIDMOVES),
+    choose_best_move(GAMESTATE, PLAYER, VALIDMOVES, MOVE),
+    wait_execute,
+    % sleep(3),
     [YS-XS, YF-XF] = MOVE,
     format('Computer greedily moved from (~w-~w) to (~w-~w).', [YS, XS, YF, XF]), nl.
 
-choose_best_move(GAMESTATE, VALIDMOVES, MOVE) :-
+choose_best_move(GAMESTATE, PLAYER, VALIDMOVES, MOVE) :-
     findall(
         VALUE-MOVE,
         (
             member(MOVE, VALIDMOVES),
             execute_move(GAMESTATE, MOVE, NEWGAMESTATE),
-            value(NEWGAMESTATE, VALUE)
+            value(NEWGAMESTATE, PLAYER, VALUE)
         ),
         MOVESVALUE
     ),
     keysort(MOVESVALUE, SORTEDMOVESVALUE),
     last(SORTEDMOVESVALUE, MAX-_),
     findall(MOVES, member(MAX-MOVES, SORTEDMOVESVALUE), MAXMOVES),
-    random_choice(MOVE, MAXMOVES).
-    % print_sorted_moves(SORTEDMOVESVALUE).  
+    random_choice(MOVE, MAXMOVES),
+    print_sorted_moves(SORTEDMOVESVALUE).  
 
-get_computer_answer(GAMESTATE, 1) :-
+get_computer_answer(GAMESTATE, _, 1) :-
     write('Since you made a jump and the jumped piece can move again, you are allowed to play again.\n'),
     write('Do you want to play again (yes or no)?\n'),
     random_choice(ANSWER, ['yes', 'no']),
@@ -69,17 +69,17 @@ get_computer_answer(GAMESTATE, 1) :-
         play_game(NEWGAMESTATE)
     ).
 
-get_computer_answer(GAMESTATE, 2) :-
+get_computer_answer(GAMESTATE, PLAYER, 2) :-
     write('Since you made a jump and the jumped piece can move again, you are allowed to play again.\n'),
     write('Do you want to play again (yes or no)?\n'),
-    choose_best_answer(GAMESTATE, ANSWER),
+    choose_best_answer(GAMESTATE, PLAYER, ANSWER),
     format('computer greedily chose: ~w.\n', [ANSWER]),
     (
     ANSWER = 'yes' ->
         play_game(GAMESTATE)
     ;
     ANSWER = 'no' ->
-        [BOARD, SIZE, PLAYER, GAMEMODE, BOTLEVEL] = GAMESTATE,
+        [BOARD, SIZE, _, GAMEMODE, BOTLEVEL] = GAMESTATE,
         allow_single_steps(PLAYER),
         clear_blocked_positions(PLAYER),
         remove_continousmove_piece(PLAYER),
@@ -88,10 +88,10 @@ get_computer_answer(GAMESTATE, 2) :-
         play_game(NEWGAMESTATE)
     ).
 
-choose_best_answer(GAMESTATE, ANSWER) :-
-    value(GAMESTATE, CURRENTVALUE),
-    valid_moves(GAMESTATE, VALIDMOVES),
-    choose_best_move(GAMESTATE, VALIDMOVES, MOVE),
+choose_best_answer(GAMESTATE, PLAYER, ANSWER) :-
+    value(GAMESTATE, PLAYER, CURRENTVALUE),
+    valid_moves(GAMESTATE, PLAYER, VALIDMOVES),
+    choose_best_move(GAMESTATE, PLAYER, VALIDMOVES, MOVE),
     execute_move(GAMESTATE, MOVE, NEWGAMESTATE),
     value(NEWGAMESTATE, FUTUREVALUE),
     (FUTUREVALUE > CURRENTVALUE ->
