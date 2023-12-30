989240b686f599ee62037d451bf1c44c37a2d24c
joaooliveira-11 <up202108737@edu.fe.up.pt>
2023-12-30 20:36:12 +0000

    if-then-else tests with () working

--- project2/Parser.hs
+++ project2/Parser.hs
@@ -3,6 +3,7 @@ import Compiler
 import Data.Char
 
 import Aux
+import Debug.Trace
 
 lexer :: String -> [Token]
 lexer [] = []
@@ -66,44 +67,69 @@ parseParentSumsOrMultOrBasicExpr tokens =
 
 parseBexp :: [Token] -> Maybe (Bexp, [Token])
 parseBexp (VarToken var : BLeToken : tokens) =
-  case parseParentSumsOrMultOrBasicExpr  tokens of
+  case parseParentSumsOrMultOrBasicExpr tokens of
     Just (aexp, restTokens) -> Just (BLe (Variable var) aexp, restTokens)
-    _ -> Nothing
+    Nothing -> Nothing
 parseBexp _ = Nothing
+
 
 parseIfThenElse :: [Token] -> Maybe (Stm, [Token])
 parseIfThenElse (IfToken : tokens1) =
   case parseBexp tokens1 of
     Just (bexp, ThenToken : tokens2) ->
       case parseStm tokens2 of
+        (thenStm, ElseToken : OpenPToken : tokens3) ->
+          case parseStms tokens3 of
+            (elseStms, ClosePToken : SemiColonToken : restTokens) -> Just (IfThenElse bexp thenStm (Seq elseStms), restTokens)
+            _ -> Nothing
         (thenStm, ElseToken : tokens3) ->
           case parseStm tokens3 of
             (elseStm, restTokens) -> Just (IfThenElse bexp thenStm elseStm, restTokens)
             _ -> Nothing
         _ -> Nothing
     _ -> Nothing
-parseIfThenElse _ = Nothing
 
+{-
+
+parseStms :: [Token] -> ([Stm], [Token])
+parseStms tokens = 
+  case parseStm tokens of
+    (stm, restTokens) -> 
+      let (stms, finalTokens) = parseStms restTokens
+      in (stm : stms, finalTokens)
+    -- (stm, restTokens) -> ([stm], restTokens)
+    -}
+
+parseStms :: [Token] -> ([Stm], [Token])
+parseStms tokens = 
+  case tokens of
+    (ClosePToken : _) -> ([], tokens)
+    _ -> case parseStm tokens of
+            (stm, restTokens) -> 
+              let (stms, finalTokens) = parseStms restTokens
+              in (stm : stms, finalTokens)
+              
 parseStm :: [Token] -> (Stm, [Token])
 parseStm (VarToken var : AssignToken : tokens) =
   case parseParentSumsOrMultOrBasicExpr  tokens of
-    Just (aexp, SemiColonToken : rest') -> (Assign var aexp, rest')
+    Just (aexp, SemiColonToken : rest') -> trace ("Remaining tokens test1: " ++ show rest') (Assign var aexp, rest')
     _ -> error "Missing semicolon or invalid syntax in arithmetic expression"
 parseStm tokens@(IfToken : _) =
   case parseIfThenElse tokens of
-    Just (stm, restTokens) -> (stm, restTokens)
+    Just (stm, restTokens) -> trace ("Remaining tokens test2: " ++ show restTokens) (stm, restTokens)
     _ -> error "Invalid syntax in if then else statement"
 parseStm _ = error "Invalid syntax"
 
+-- testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("","x=34")
 
 parseTokens :: [Token] -> Program
 parseTokens [] = []
-parseTokens tokens = let (stm, rest) = parseStm tokens
-                     in stm : parseTokens rest
+parseTokens tokens = let (stm, tailtoken) = parseStm tokens
+                     in stm : parseTokens tailtoken
 
 parse :: String -> Program
-parse str = 
-  case lexer str of
+parse input = 
+  case lexer input of
     tokens ->
       case parseTokens tokens of
         [] -> error "Empty program"
