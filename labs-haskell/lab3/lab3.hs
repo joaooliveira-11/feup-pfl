8ab2debbe6e4ebee2b2a73a94c390b0e06c060db
joaooliveira-11 <up202108737@edu.fe.up.pt>
2024-01-04 18:50:34 +0000

    lab3 done

--- labs-haskell/lab3/lab3.hs
+++ labs-haskell/lab3/lab3.hs
@@ -1,60 +1,84 @@
 import Data.List (foldl1)
-
+import Data.List (foldr1)
+import Data.List (insert)
+import Data.Char (isSpace)
 
 -- 3.1)
--- [f x | x ← xs, p x] s
-mapFilter :: (a -> b) -> (a -> Bool) -> [a] -> [b]
-mapFilter f p xs = map f (filter p xs)
+
+-- [f x | x ← xs, p x] 
+
+myFilter :: (a -> a) -> (a -> Bool) -> [a] -> [a]
+myFilter func filt list = map func (filter filt list)
 
 -- 3.2)
-dec2int :: [Int] -> Int
-dec2int list = foldl (\acc x -> acc * 10 + x) 0 list
+
+myDec2int :: [Int] -> Int
+myDec2int list = foldl (\acc x -> acc * 10 + x) 0 list
+
 
 -- 3.3)
--- zipWith :: (a → b → c) → [a] → [b] → [c]
--- zipWith f xs ys = [f x y | (x, y) ← zip xs ys
-
-zipWithRec :: (a -> b -> c) -> [a] -> [b] -> [c] 
-zipWithRec _ [] _ = [] 
-zipWithRec _ _ [] = [] 
+zipWithRec :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWithRec _ [] _ = []
+zipWithRec _ _ [] = []
 zipWithRec f (h1:t1) (h2:t2) = f h1 h2 : zipWithRec f t1 t2
 
 -- 3.4)
+myInsert :: Ord a => a -> [a] -> [a]
+myInsert el [] = [el]
+myInsert el (h:t) = 
+    if el <= h then el : h : t
+    else h: myInsert el t 
 
-{- from lab 2
+myIsort :: Ord a => [a] -> [a]
+myIsort [] = []
+myIsort (h:t) = myInsert h (myIsort t)
 
--- 2.4)
+foldIsort :: Ord a =>  [a] -> [a]
+foldIsort list = foldr myInsert [] list
 
--- a)
-insert_2 :: Ord a => a -> [a] -> [a]
-insert_2 a [] = [a]
-insert_2 a (h:t)
-    | a <= h = a : h : t
-    | otherwise = h : insert_2 a t
 
--- b)
-isort_2 :: Ord a => [a] -> [a]
-isort_2 [] = []
-isort_2 (h:t) = insert_2 h (isort_2 t)
+foldIsort1 :: Ord a =>  [a] -> [a]
+foldIsort1 list = foldr (\x acc -> myInsert x acc) [] list
 
-isort :: Ord a ⇒ [a] → [a]
--}
 
-insert_2 :: Ord a => a -> [a] -> [a]
-insert_2 a [] = [a]
-insert_2 a (h:t)
-    | a <= h = a : h : t
-    | otherwise = h : insert_2 a t
+-- 3.5)
 
-isortFoldr :: Ord a => [a] -> [a]
-isortFoldr list = foldr insert_2 [] list
+maximumLeft :: Ord a => [a] -> a
+maximumLeft list = foldl1 (\acc x -> max acc x) list
 
+maximumRight :: Ord a => [a] -> a
+maximumRight list = foldr1 (\x acc -> max acc x) list
+
+minimumLeft :: Ord a => [a] -> a
+minimumLeft list = foldl1 (\acc x -> min acc x) list
+
+minimumRight :: Ord a => [a] -> a
+minimumRight list = foldr1 (\x acc -> min acc x) list
 
 -- 3.6)
 
-untilMdc :: Integer -> Integer -> Integer
-untilMdc a b = fst(until (\(_, b) -> b == 0) (\(a, b) -> (b, a `mod` b)) (a, b))
 
 -- 3.7)
 
 -- a)
+concat2Foldr :: [a] -> [a] -> [a]
+concat2Foldr list1 list2 = foldr (\x acc -> x : acc) list2 list1
+
+-- b)
+concatFoldr :: [[a]] -> [a]
+concatFoldr lists = foldr (\x acc -> concat2Foldr x acc) [] lists
+
+-- c)
+reverseFoldr :: [a] -> [a]
+reverseFoldr list = foldr (\x acc -> acc ++ [x]) [] list
+
+-- d)
+reverseFoldl :: [a] -> [a]
+reverseFoldl list = foldl (\acc x -> [x] ++ acc) [] list
+
+-- e)
+elemAny :: Eq a => a -> [a] -> Bool
+elemAny el list = any (\x -> x == el) list
+
+
+-- 3.8)
